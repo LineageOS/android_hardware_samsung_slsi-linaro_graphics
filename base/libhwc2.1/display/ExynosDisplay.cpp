e56fdc724012403991cb9fa0818f45874c97fde9
ExtremeXT <extremextdev@gmail.com>
2025-12-07 22:32:19 +0200

    libhwc2.1: Fetch UDFPS Z-Order from samsungUdfpsVars
    Change-Id: I306ccc754097f31eeb302020de8554910ec29c20
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- base/libhwc2.1/display/ExynosDisplay.cpp
+++ base/libhwc2.1/display/ExynosDisplay.cpp
@@ -1849,13 +1849,17 @@ int ExynosDisplay::deliverWinConfigData(DevicePresentInfo &presentInfo) {
         ret = 0;
     } else {
 #ifdef HAS_FINGERPRINT_MASK_LAYER
-        for (size_t i = mLayers.size(); i-- > 0; ) {
-            if (mLayers[i]->mZOrder == EXYNOS_UDFPS_PRESSED_LAYER_ZORDER) {
-                for (size_t j = 0; j < mDpuData.configs.size(); j++) {
-                    if (mDpuData.configs[j].state == mDpuData.configs[j].WIN_STATE_BUFFER) {
-                        mDpuData.configs[j].state = mDpuData.configs[j].WIN_STATE_FINGERPRINT;
-                        HDEBUGLOGD(eDebugWinConfig, "Sent UDFPS Mask layer command to DECON");
-                        break;
+        uint32_t udfpsZorder = UDFPS_DIM_LAYER_ZORDER ? UDFPS_DIM_LAYER_ZORDER :
+                        (UDFPS_PRESSED_LAYER_ZORDER ? UDFPS_PRESSED_LAYER_ZORDER : 0);
+        if (udfpsZorder) {
+            for (size_t i = mLayers.size(); i-- > 0; ) {
+                if (mLayers[i]->mZOrder == udfpsZorder) {
+                    for (size_t j = 0; j < mDpuData.configs.size(); j++) {
+                        if (mDpuData.configs[j].state == mDpuData.configs[j].WIN_STATE_BUFFER) {
+                            mDpuData.configs[j].state = mDpuData.configs[j].WIN_STATE_FINGERPRINT;
+                            HDEBUGLOGD(eDebugWinConfig, "Sent UDFPS Mask layer command to DECON");
+                            break;
+                        }
                     }
                 }
             }
