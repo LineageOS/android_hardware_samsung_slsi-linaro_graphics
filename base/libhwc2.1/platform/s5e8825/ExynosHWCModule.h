ee7a4bdede4be1f9771f92556979582875135a36
ExtremeXT <extremextdev@gmail.com>
2025-12-11 17:44:59 +0200

    libhwc2.1: s5e8825: Set stock hardware clocks
    Change-Id: I994a49dabd11ed1e88b1887a6f07656da223e616
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- base/libhwc2.1/platform/s5e8825/ExynosHWCModule.h
+++ base/libhwc2.1/platform/s5e8825/ExynosHWCModule.h
@@ -103,7 +103,12 @@ struct exynos_display_t {
 #define EXTERNAL_MAIN_EXTERNAL_WINCNT  2
 #define PRIMARY_MAIN_VIRTUAL_WINCNT 2
 #define DEFAULT_MPP_DST_YUV_FORMAT HAL_PIXEL_FORMAT_EXYNOS_YCbCr_420_SPN_SBWC
-#define MSC_CLOCK   663000
+#define MSC_CLOCK   800000
+
+#define VPP_CLOCK       800000000
+#define VPP_PIXEL_PER_CLOCK 2
+
+#define G2D_CLOCK       712024
 
 enum {
     DISPLAY_MODE_PRIMARY_MAIN = 0,  /* This is default mode */
